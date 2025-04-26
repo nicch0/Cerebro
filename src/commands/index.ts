0e065bf8e8893b94f5118bf38920a8aefd99c6c0
Nick Chong <nchong128@gmail.com>
2025-04-26 19:27:32 +1000

    refactor: remove unused v1 commands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,23 +1,14 @@
 import type { Command } from "obsidian";
 import Cerebro from "../main";
 import { chatCommand } from "./chat";
-import { clearChatCommand } from "./clear";
-import { addCommentBlockCommand, addDividerCommand } from "./formatting";
 import { inferTitleCommand } from "./inferTitle";
 import { createNewChatCommand, createNewChatInSidebarCommand } from "./newChat";
-import { stopStreamingCommand } from "./stopStreaming";
-import { switchModelCommand } from "./switchModel";
 import { chooseChatTemplateCommand } from "./template";
 
 export const getCommands = (plugin: Cerebro): Command[] => [
     createNewChatCommand(plugin),
     createNewChatInSidebarCommand(plugin),
     chatCommand(plugin),
-    addDividerCommand(plugin),
-    addCommentBlockCommand(plugin),
     inferTitleCommand(plugin),
     chooseChatTemplateCommand(plugin),
-    clearChatCommand(plugin),
-    stopStreamingCommand(plugin),
-    switchModelCommand(plugin),
 ];
