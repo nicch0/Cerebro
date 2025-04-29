33381e821c170e7e05a776340a80518c69fd81bc
Nick Chong <nchong128@gmail.com>
2025-04-29 15:38:09 +1000

    feat: basic save user conversation upon close

--- src/components/ui/chat/chat-bubble/index.d.ts
+++ src/components/ui/chat/chat-bubble/index.d.ts
@@ -1,9 +1,9 @@
-import { chatBubbleMessageVariants } from "./chat-bubble-message.svelte";
 import { chatBubbleVariant } from "./chat-bubble.svelte";
+import { chatBubbleMessageVariants } from "./chat-bubble-message.svelte";
 
+export { default as ChatBubble } from "./chat-bubble.svelte";
 export { default as ChatBubbleActionWrapper } from "./chat-bubble-action-wrapper.svelte";
 export { default as ChatBubbleAction } from "./chat-bubble-actions.svelte";
 export { default as ChatBubbleMessage } from "./chat-bubble-message.svelte";
 export { default as ChatBubbleTimestamp } from "./chat-bubble-timestamp.svelte";
-export { default as ChatBubble } from "./chat-bubble.svelte";
 export { chatBubbleMessageVariants, chatBubbleVariant };
