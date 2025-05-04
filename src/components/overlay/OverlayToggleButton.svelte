c5eec26a028636d03698927e259931f15c0eb453
Nick Chong <nchong128@gmail.com>
2025-05-05 00:06:33 +1000

    feat: replace toggle button with svelte component

---
+++ src/components/overlay/OverlayToggleButton.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { BrainCircuit } from "@lucide/svelte";
+    type Props = {
+        toggleActive: () => void;
+        data: { active: boolean };
+    };
+
+    let { data, toggleActive }: Props = $props();
+</script>
+
+<button
+    aria-label="Toggle Cerebro Overlay"
+    class={[
+        "cerebro-overlay-button",
+        "clickable-icon",
+        "view-action",
+        data.active && ["pinned", "button-active"],
+    ]}
+    variant="outline"
+    size="default"
+    onclick={toggleActive}
+>
+    <BrainCircuit class="size-4" />
+</button>
